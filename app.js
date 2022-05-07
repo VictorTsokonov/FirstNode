40a0430e459047932d4473497668e30e26cf166f
victor_ts <victorcodingacc@gmail.com>
2022-05-07 19:35:37 +0300

    Deleted Pug and HandleBars engines and templates, forwarding to EJS template engine

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const path = require("path");
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir:path.join(__dirname, '/views/layouts/'), defaultLayout: 'main-layout', extname: 'hbs'})); // expressHbs() initializes the ENGINE !
-app.set('view engine', 'hbs'); // <-- IMPORTANT! --> that's how you initialize the file extension also: .hbs
+app.set('view engine', 'ejs'); // <-- IMPORTANT! --> that's how you initialize the file extension also: .hbs
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended:true}));
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,14 @@
-const path = require("path");
-
 const express = require('express');
 
-const rootDir = require('../utils/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get("/",(req, res, next) => {
-    // ...
-    // console.log(adminData.products)
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    const hasProducts = adminData.products.length > 0;
     res.render('shop', {
-         prods: adminData.products,
+         products: adminData.products,
          pageTitle: "Shop",
          path: "/",
-         hasProducts: hasProducts,
          activeShop: true,
          productCSS: true
     });
