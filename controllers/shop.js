6c470c8505be4b21d9a87d32d1415fea7d7d8c78
victor_ts <victorcodingacc@gmail.com>
2022-05-08 20:28:18 +0300

    update

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,43 +1,42 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/products-list', {
-            products: products,
-            pageTitle: "Products",
-            path: "/products",
-       });
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
     });
-}
-
-exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Cart'
-    });
-};
-
-exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'orders'
-    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/index', {
-            products: products,
-            pageTitle: "Shop",
-            path: "/index",
-       });
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
     });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
 };
 
 exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout',  {
-        path: '/checkout',
-        pageTitle: 'Checkout'
-    });
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
 };
-    
--- utils/path.js
+++
@@ -1,3 +0,0 @@
-const path = require("path");
-
-module.exports = path.dirname(process.mainModule.filename); // If doesn't work, use --> module.exports = path.dirname(require.mainModule.filename);
