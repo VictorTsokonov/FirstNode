37911e0ed34aa20acd2d58d1de3d6c3e714c8a0a
victor_ts <victorcodingacc@gmail.com>
2022-05-09 22:35:24 +0300

    update

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -7,6 +9,17 @@ exports.getProducts = (req, res, next) => {
       pageTitle: 'All Products',
       path: '/products'
     });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId;
+  Product.findById(productId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
   });
 };
 
@@ -26,6 +39,14 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, product.price)
+  });
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
---
+++ models/cart.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const Product = require('./product');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice){
+        fs.readFile(p, (err, data) => {
+            let cart = {products: [], totalPrice: 0};
+            if(!err){
+                cart = JSON.parse(data);
+            }
+            const existingProductIndex = cart.products.findIndex(product => {return product.id === id});
+            if(existingProductIndex !== -1){
+                cart.products[existingProductIndex].qty += 1;
+            }else{
+                cart.products.push({id: id, qty: 1});
+            }
+            cart.totalPrice += +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+
+}
