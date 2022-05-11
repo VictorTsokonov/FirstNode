8f91ad7c821e17e1ad1bc942cb0c56308fa35cd6
victor_ts <victorcodingacc@gmail.com>
2022-05-11 23:28:09 +0300

    Database initialization in development

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'D$B4se',
-    port:3306
-})
+const sequelize = new Sequelize("node-complete", "root", "D$B4se", {
+  dialect: "mysql",
+  host: "localhost",
+});
 
-module.exports = pool.promise();
+module.exports = sequelize;
