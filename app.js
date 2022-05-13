220bfd08051a432da803b5e477daeb026bd640f5
victor_ts <victorcodingacc@gmail.com>
2022-05-13 22:11:30 +0300

    update

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 
+
 app.use((req, res, next)=>{
   User.findByPk(1)
   .then((user) => {
